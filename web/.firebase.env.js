b8febbb4f991421720e89bb05a7049514245848b
ydideh810 <yd.baller810@gmail.com>
2024-07-17 13:05:16 -0400

    Update and rename .firebase.env.example.js to .firebase.env.js

---
+++ web/.firebase.env.js
@@ -0,0 +1,8 @@
+module.exports = {
+ apiKey: "AIzaSyBkxVwmkL8WV_eRpSZHoWj2zGW4q0U6hf4",
+  authDomain: "mapwrld-1.firebaseapp.com",
+  projectId: "mapwrld-1",
+  storageBucket: "mapwrld-1.appspot.com",
+  messagingSenderId: "611533888292",
+  appId: "1:611533888292:web:82b9f803268145acce31b1"
+};
